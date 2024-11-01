455a2fcb4bf784dae9f09f241bcd4ed887522315
Chiranjeevi <nchiranjeevi987@gmail.com>
2024-11-01 20:38:39 +0530

    Added docker file, docker compose file for sql and node containers

---
+++ app/index.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+db.connect(err => {
+  if (err) throw err;
+  console.log('Connected to MySQL Database');
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World! Node.js is running!');
+});
+
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
