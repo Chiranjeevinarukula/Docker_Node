77b8401f8fdaff8b0a9b885221fba6a04fe7fb76
Chiranjeevi <nchiranjeevi987@gmail.com>
2024-11-15 09:13:11 +0530

    Added printrecipt get & post request (#2)

--- app/index.js
+++ app/index.js
@@ -15,6 +15,9 @@ db.connect(err => {
   console.log('Connected to MySQL Database');
 });
 
+app.use(express.json());
+
+
 app.get('/', (req, res) => {
   res.send('Hello World! Node.js is running!');
 });
@@ -27,8 +30,23 @@ app.get('/users',(req,res)=>{
     }
     res.send(result);
   })
-})
+});
+
+var base64 = "";
+app.get('/printreceipt',(req,res)=>{
+  res.send(base64);
+});
+
+app.post('/printreceipt', (req, res) => {
+  console.log("POST request to /printreceipt");
+  if (!req.body.base64) {
+    return res.status(400).send("Base64 data is required");
+  }
+  base64 = {"print recipt":req.body.base64};  // Store the base64 data
+  res.send("Successfully done!!");
+});
 
 app.listen(port, () => {
   console.log(`App running on http://localhost:${port}`);
 });
+
